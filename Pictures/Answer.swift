b4d233684bd9cfcbb6e96f1ab1df4e8b11a3aa92
BillyLiao <hohey670@gmail.com>
2015-12-25 11:06:38 +0800

    first commit

---
+++ Pictures/Answer.swift
@@ -0,0 +1,9 @@
+//
+//  Answer.swift
+//  Pictures
+//
+//  Created by 廖慶麟 on 2015/12/23.
+//  Copyright © 2015年 廖慶麟. All rights reserved.
+//
+
+import Foundation
---
+++ Pictures/AnswerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnswerTableViewCell.swift
+//  Pictures
+//
+//  Created by 廖慶麟 on 2015/12/23.
+//  Copyright © 2015年 廖慶麟. All rights reserved.
+//
+
+import UIKit
+
+class AnswerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
